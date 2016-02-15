a76d08ad81c6855f05e88dd2241514132b21f59d
lennartquerter <lennartquerter@surfer-172-29-108-17-hotspot.s-bit.nl>
2016-02-15 21:26:05 +0100

    controllers and stuff

--- js/bomberman_app.js
+++ js/bomberman_app.js
@@ -1,3 +1,7 @@
 /**
  * Created by lennartquerter on 15/02/16.
  */
+
+
+
+var app = angular.module('myApp', []);
--- js/bomberman_ctrl.js
+++ js/bomberman_ctrl.js
@@ -1,3 +1,11 @@
 /**
  * Created by lennartquerter on 15/02/16.
  */
+
+
+
+app.controller('myCtrl', function($scope) {
+
+
+
+});
