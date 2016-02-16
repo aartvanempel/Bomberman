b0a22f0335110c14b92ccc781c0e0a122a7b4222
lennartquerter <lennartquerter@Lennarts-MacBook-Pro.local>
2016-02-16 14:00:41 +0100

    random rocks generator

--- js/grid_ctrl.js
+++ js/grid_ctrl.js
@@ -2,6 +2,7 @@ app.controller('gridCtrl', function($scope) {
 
 
 
+  var rockPositions = randomCoordinate("0-0");
 
 
   $scope.createGrid = function() {
@@ -9,46 +10,18 @@ app.controller('gridCtrl', function($scope) {
       for (var x = 0; x < 15; x++) {
         if (([1, 3, 5, 7, 9, 11, 13].indexOf(x)) != -1 && ([1, 3, 5, 7, 9, 11, 13].indexOf(y)) != -1) {
           $(".grid-container").append("<div class='bedrock cell " + x + "-" + y + "'></div>");
-        } else {
+        } else if (
+           rockPositions.indexOf((x + "-" + y)) == -1
+        ){
+          $(".grid-container").append("<div class='rock cell " + x + "-" + y + "'></div>");
+        }
+        else {
           $(".grid-container").append("<div class='cell " + x + "-" + y + "'></div>");
         }
       }
     }
 
-    //make bedrock items -Class bedrock
-
-    var $notBedrock = $('.cell').not(".bedrock");
-    console.log($notBedrock);
-    var $bedrock = $(".bedrock");
-
-    $notBedrock.each(function() {
-
-      if ($(this).hasClass("current-cell")) {
-        console.log("has Class")
-      } else {
-        $(this).addClass("rock")
-      }
-
-
-
-    });
-
-
-
-
-
-
-
-    //vanaf 3-0 && 0-3
-
-    //make random destroyable blocks --class rocks
-
-    //class = rock en dat moet op
-
-
     //create random power ups when destroying block (1 kans op 8 per upgrade?)
-
-
 
   };
 
@@ -56,14 +29,34 @@ app.controller('gridCtrl', function($scope) {
 
 
 
-/*
-*  1
-*  3
-*  5
-*  7
-*  9
-*  11
-*  13
-*  15
-*
-* */
+function randomCoordinate(spawnPosition) {
+  var randomX;
+  var randomY;
+  var randomLenght = getRandomInt(2, 10);
+  var randomPosition;
+  var randomPositionList = [];
+
+
+  //start positions (vul hier misschien direcht spawn positions ook in ??
+
+  randomPositionList = ["0-1", "0-2", "0-3", "1-0", "2-0", "3-0", "1-2", "2-1"];
+  randomPositionList.push(spawnPosition);
+
+
+  for (var i = 0; i < randomLenght; i++) {
+    randomX = getRandomInt(0, 15);
+    randomY = getRandomInt(0, 15);
+    randomPosition = randomX + "-" + randomY;
+    randomPositionList.push(randomPosition);
+  }
+
+
+  console.log(randomPositionList);
+  return randomPositionList;
+
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
