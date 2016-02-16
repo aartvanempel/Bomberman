9845de0d3f984f6d554dfe70b559497a96a15dd1
lennartquerter <lennartquerter@Lennarts-MacBook-Pro.local>
2016-02-16 11:01:58 +0100

    grid update

--- js/grid_ctrl.js
+++ js/grid_ctrl.js
@@ -1,11 +1,48 @@
 app.controller('gridCtrl', function($scope) {
 
+
+
+
+
   $scope.createGrid = function() {
-      for(var x = 0; x < 16; x++) {
-        for(var y = 0; y < 16; y++) {
-          $(".grid-container").append("<div class='cell " + x + "-" + y + "'></div>");
+      for(var x = 0; x < 15; x++) {
+        for(var y = 0; y < 15; y++) {
+          if (([1,3,5,7,9,11,13].indexOf(x)) != -1 && ([1,3,5,7,9,11,13].indexOf(y)) != -1) {
+            $(".grid-container").append("<div class='bedrock cell " + x + "-" + y + "'></div>");
+          } else {
+            $(".grid-container").append("<div class='cell " + x + "-" + y + "'></div>");
+          }
+
         }
       }
+
+    //make bedrock items -Class bedrock
+
+
+
+    //make random destroyable blocks --class rocks
+
+
+
+
+    //create random power ups when destroying block (1 kans op 8 per upgrade?)
+
+
+
   };
 
 });
+
+
+
+/*
+*  1
+*  3
+*  5
+*  7
+*  9
+*  11
+*  13
+*  15
+*
+* */
